29361ff85cf91a25214c931959257df1a47442d1
bala0546 <165453858+bala0546@users.noreply.github.com>
2025-02-10 10:21:06 +0530

    Delete CoE_JavaFSD-main/src/week2/inventoryManagementSystem directory

--- CoE_JavaFSD-main/src/week2/inventoryManagementSystem/InvalidLocationException.java
+++
@@ -1,7 +0,0 @@
-package week2.inventoryManagementSystem;
-
-public class InvalidLocationException extends Exception {
-    public InvalidLocationException(String message) {
-        super(message);
-    }
-}
--- CoE_JavaFSD-main/src/week2/inventoryManagementSystem/OutOfStockException.java
+++
@@ -1,8 +0,0 @@
-package week2.inventoryManagementSystem;
-
-public class OutOfStockException extends Exception {
-    public OutOfStockException(String message) {
-        super(message);
-    }
-}
-
